2af8c929254d16013b86968e96fad841e9f726da
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 16:37:33 +0530

    Working with Literal Types.

--- app.ts
+++ app.ts
@@ -1,15 +1,26 @@
-function combine(number1: number | string, number2: number | string) {
+function combine(number1: number | string, number2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
-    if (typeof number1 === 'number' && typeof number2 === 'number') {
-        result = number1 + number2;
+    if (typeof number1 === 'number' && typeof number2 === 'number' || resultConversion === 'as-number') {
+        result = +number1 + +number2;
     } else {
         result = number1.toString() + number2.toString();
     }
+
     return result;
+
+    // if (resultConversion === 'is-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString();
+    // }
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Sajed', 'Shaikh')
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+
+const combineNames = combine('Sajed', 'Shaikh', 'as-text')
 console.log(combineNames);
