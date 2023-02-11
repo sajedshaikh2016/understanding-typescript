a49436199d204f6647314bb126e158bc75b03c44
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 15:04:13 +0530

    Working with Enums.

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Sajed",
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'Author']
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: "Sajed",
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'Author']
+    role: Role.AUTHOR
 };
 var favoraitActivity;
 favoraitActivity = ['Learning'];
@@ -11,4 +32,10 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log("Is admin");
+}
+if (person.role === Role.AUTHOR) {
+    console.log("Is author");
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,28 @@
-const person: {
-    name: string,
-    age: number,
-    hobbies: string[],
-    role: [number, string]
-} = {
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Sajed",
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'Author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+    ADMIN, READ_ONLY, AUTHOR
+};
+
+const person = {
     name: "Sajed",
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'Author']
+    role: Role.AUTHOR
 };
 
 let favoraitActivity: string[];
@@ -18,4 +33,13 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+    console.log("Is admin");
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log("Is author");
+
 }
