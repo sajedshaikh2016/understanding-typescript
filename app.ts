29784047fcbebaa5434bba3ead9039037b2552ff
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 15:18:03 +0530

    Working with Union Types.

--- app.ts
+++ app.ts
@@ -1,14 +1,15 @@
-function combine(number1, number2) {
-    var result;
+function combine(number1: number | string, number2: number | string) {
+    let result;
     if (typeof number1 === 'number' && typeof number2 === 'number') {
         result = number1 + number2;
-    }
-    else {
+    } else {
         result = number1.toString() + number2.toString();
     }
     return result;
 }
-var combineAges = combine(30, 26);
+
+const combineAges = combine(30, 26);
 console.log(combineAges);
-var combineNames = combine('Sajed', 'Shaikh');
+
+const combineNames = combine('Sajed', 'Shaikh')
 console.log(combineNames);
