46629d87fd8e258b58dffa377c7ccc3089a04aae
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 17:53:56 +0530

    The "never" return type

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ userInput = "Sajed";
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occured!", 500);
--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ userInput = "Sajed"
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code };
+}
+
+generateError("An error occured!", 500);
