a43dcdb9f944f191838639ceaada8750e456f157
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 18:06:02 +0530

    Compiling the entire typescript project and all files.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var userInput;
-var userName;
+"use strict";
+let userInput;
+let userName;
 userInput = 5;
 userInput = "Sajed";
 if (typeof userInput === 'string') {
--- basics.js
+++ basics.js
@@ -1,17 +1,15 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-
+"use strict";
+function add(n1, n2, showResult, phrase) {
     const result = n1 + n2;
     if (showResult) {
         console.log(phrase + result);
-    } else {
+    }
+    else {
         return result;
     }
 }
-
 const number1 = 2;
 const number2 = 3.5;
 const printResult1 = true;
 const resultPhrase = "Result is : ";
-
 add(number1, number2, printResult1, resultPhrase);
-
