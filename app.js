3c186b87e54acbde583930f04b76df6d07fa0035
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 17:29:39 +0530

    Function types & callbacks

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ function addition(n1, n2) {
 function printResult(num) {
     console.log("Result : ", num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(addition(10, 15));
 var combineValues;
 combineValues = addition;
@@ -11,3 +15,6 @@ combineValues = addition;
 // combineValues = 5;
 console.log(combineValues(8, 2));
 // let someValue: undefined;
+addAndHandle(10, 22, function (result) {
+    console.log(result);
+});
