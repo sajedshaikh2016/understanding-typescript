c3210967216087ef59550b0bf9e61a8bad9d509b
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 13:47:46 +0530

    Working with Array Types.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var person = {
     name: "Sajed",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person);
+var favoraitActivity;
+favoraitActivity = ['Learning'];
+// Added a comment
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
 const person = {
     name: "Sajed",
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoraitActivity: string[];
+favoraitActivity = ['Learning'];
+
 // Added a comment
-console.log(person);
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
