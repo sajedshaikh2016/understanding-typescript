840bc2608d84983cd9c8e4917ed25863ee00953a
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 16:46:32 +0530

    Type Alias / Custom Types.

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-function combine(number1: number | string, number2: number | string, resultConversion: 'as-number' | 'as-text') {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(number1: Combinable, number2: Combinable, resultConversion: ConversionDescriptor) {
     let result;
     if (typeof number1 === 'number' && typeof number2 === 'number' || resultConversion === 'as-number') {
         result = +number1 + +number2;
