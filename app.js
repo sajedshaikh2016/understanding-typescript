4023c4f07516f9927eb8f98cb8801ae38b02bf89
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 17:08:53 +0530

    function return types & void.

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-function combine(number1, number2, resultConversion) {
-    var result;
-    if (typeof number1 === 'number' && typeof number2 === 'number' || resultConversion === 'as-number') {
-        result = +number1 + +number2;
-    }
-    else {
-        result = number1.toString() + number2.toString();
-    }
-    return result;
-    // if (resultConversion === 'is-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+function addition(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combineNames = combine('Sajed', 'Shaikh', 'as-text');
-console.log(combineNames);
+function printResult(num) {
+    console.log("Result : ", num);
+}
+addition(10, 15);
---
+++ app.ts
@@ -0,0 +1,9 @@
+function addition(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+function printResult(num: number): void {
+    console.log("Result : ", num);
+}
+
+addition(10, 15);
