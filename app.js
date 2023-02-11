97730abb02852a09689279bb1334791f3ae2c110
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-02-11 17:38:25 +0530

    the unknown type

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-function addition(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Sajed";
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result : ", num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(addition(10, 15));
-var combineValues;
-combineValues = addition;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 2));
-// let someValue: undefined;
-addAndHandle(10, 22, function (result) {
-    console.log(result);
-});
